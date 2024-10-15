06844272bbe7d64cb1dd912baf26ff76445c76a1
rick30-8-2024 <iamrick21@gmail.com>
2024-10-15 23:06:51 +0000

    modified

--- Organizer.py
+++ Organizer.py
@@ -13,6 +13,7 @@ class Organizer:
         self.session = requests.session()
         self.keys = ['a83c8583-a310-44e1-ac82-75787a5d8dd5', 'fc966f2d-11a4-4b5c-87f6-c9e0d19869b0', 'bb909ed0-6f67-4131-be88-c731e1602659', 'f3269905-5dc2-4e3c-9181-6a155e019b85', 'ba327648-f98c-4f89-bbb2-6f82f9f53d0d', '348e3e5c-68ef-4bfd-94ff-a8da9112de21', 'fb0722f1-572e-41f0-aef7-bc70cb3b097e', '451dc795-c196-4a63-be92-54617da8da45', '50a9ab67-fb0e-49d6-b67f-16b5a7e777e2', '849d8319-fbf7-42b8-a1f8-68ec5d5bc69d']
         self.all_Images = []
+        self.all_links = []
 
 
     def get_filtered_urls(self, query):
@@ -45,21 +46,38 @@ class Organizer:
         json_response = self.researcher.scrape_page(url)
         full_text = json_response['structured_data']['full_text']
         self.all_Images.extend([i['src'] for i in json_response['structured_data']['images']])
-        chunked_text = split_corpus(full_text)
+        self.all_links.extend([i['src'] for i in json_response['structured_data']['links']])
         
-        for i in range(5):
-            print(len(chunked_text))
-            text_chunk_list = [(chunked_text[m], query, self.keys[m%10]) for m in range(len(chunked_text))]
-            tasks = [self.summerize(chunk, show=True) for chunk in text_chunk_list]
+        # for i in range(5):
+        #     print(len(chunked_text))
+        #     text_chunk_list = [(chunked_text[m], query, self.keys[m%10]) for m in range(len(chunked_text))]
+        #     tasks = [self.summerize(chunk, show=True) for chunk in text_chunk_list]
+        #     results = await asyncio.gather(*tasks)
+        #     chunked_text = split_corpus("\n\n".join(results))
+        #     if len(chunked_text) == 1:
+        #         return "\n\n".join(results)
+        return full_text
+    
+    async def process_text_corpus(self, text_corpus, query):
+        chunked_text = split_corpus(text_corpus)
+        
+        while len(chunked_text) > 1:  
+            text_chunk_list = [(chunk, query, self.keys[m % len(self.keys)]) for m, chunk in enumerate(chunked_text)]
+            tasks = [self.summarize(chunk, show=True) for chunk in text_chunk_list]
             results = await asyncio.gather(*tasks)
-            chunked_text = split_corpus("\n\n".join(results))
-            if len(chunked_text) == 1:
-                return "\n\n".join(results)
+            
+            text_corpus = "\n\n".join(results)  
+            chunked_text = split_corpus(text_corpus)  
+
+        return text_corpus  
+
 
     async def search(self, query):
         filtered_urls = self.get_filtered_urls(query)
         tasks = [self.Processor(url, query) for url in filtered_urls]
         list_of_chunks = await asyncio.gather(*tasks)
+        alltext = "\n\n".join(list_of_chunks)
+        summery = await self.process_text_corpus(alltext, query)
         # print(len(final_data_list))
         # print(final_data_list)
         # final_data_list = "\n\n".join(final_data_list)
